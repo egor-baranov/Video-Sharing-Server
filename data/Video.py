5aed73f77bd693d9c24d8ddf7e1f1064e91b3344
0x5d0 <49813134+egor-baranov@users.noreply.github.com>
2020-12-03 03:30:02 +0300

    ✨ Created VideoFactory class

--- data/Video.py
+++ data/Video.py
@@ -1,13 +1,30 @@
-class Video:
-    title = ""
-    description = ""
-    tags = ""
-    size = 0
-    length = 0
-    views = 0
-    likes = 0
+import typing
 
-    comments = []
+
+class Comment:
+    pass
+
+
+class Video:
+    title: str
+    description: str
+    tags: str
+    size: int
+    length: int
+    views: int
+    likes: int
+
+    comments: typing.List[Comment] = []
+
+    def __init__(self, title: str = "", description: str = "", tags: str = "", size: int = 0, length: int = 0,
+                views: int = 0, likes: int = 0):
+        self.title = title
+        self.description = description
+        self.tags = tags
+        self.size = size
+        self.length = length
+        self.views = views
+        self.likes = likes
 
     def to_dict(self):
         return {"title": self.title,
@@ -17,5 +34,12 @@ class Video:
                 "length": self.length}
 
 
-class Comment:
-    pass
+class VideoFactory:
+    @staticmethod
+    def new_fake_video() -> Video:
+        return Video()
+
+    @staticmethod
+    def new_video(title: str, description: str, tags: str, size: int, length: int, views: int, likes: int):
+        return Video(title, description, tags, size, length, views, likes)
+
