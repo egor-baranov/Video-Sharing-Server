5167df68d3072dedc17ecb8f7536e2a3e6cf64ce
0x5d0 <49813134+egor-baranov@users.noreply.github.com>
2021-01-02 09:25:43 +0300

    ✨ Some stuff moved to DatabaseWorker

--- components/database/dbworker.py
+++ components/database/dbworker.py
@@ -1,2 +1,24 @@
+from data.User import UserFactory
+from data.Video import Video
+
+
 class DatabaseWorker:
-    pass
+
+    Users = [UserFactory.new_user(
+        username="admin",
+        password="password",
+        email="admin@admin.com",
+        birth_date="31.12.2000",
+        city="city",
+        phone="89004445533"
+    )]
+
+    BlockedUsers = []
+
+    Videos = [
+        Video(title="Видео для отладки 1", length=2374),
+        Video(title="Видео для отладки 2", length=24),
+        Video(title="Видео для отладки 3", length=123)
+    ]
+
+    Comments = []
