01219729ac50c98d96530aba4e31535c6c2c8a8e
0x5d0 <49813134+egor-baranov@users.noreply.github.com>
2021-01-03 02:59:43 +0300

    🔨 Implemented Video.comments

--- data/Video.py
+++ data/Video.py
@@ -24,7 +24,8 @@ class Video:
                  length: int = 0,
                  views: int = 0,
                  like_count: int = 0,
-                 cloudinary_id: int = 0):
+                 cloudinary_id: int = 0,
+                 comments: typing.List[Comment] = None):
         self.title = title
         self.description = description
         self.tags = tags
@@ -33,6 +34,7 @@ class Video:
         self.views = views
         self.likes = like_count
         self.cloudinary_id = cloudinary_id
+        self.comments = comments
 
     def to_dict(self):
         return {"title": self.title,
@@ -42,7 +44,8 @@ class Video:
                 "length": self.length,
                 "views": self.views,
                 "likeCount": self.likes,
-                "videoId": self.cloudinary_id}
+                "videoId": self.cloudinary_id,
+                "comments": self.comments}
 
 
 class VideoFactory:
@@ -51,8 +54,9 @@ class VideoFactory:
         return Video()
 
     @staticmethod
-    def new_video(title: str, description: str, tags: str, size: int, length: int, views: int, like_count: int, cloudinary_id: int):
-        return Video(title, description, tags, size, length, views, like_count, cloudinary_id)
+    def new_video(title: str, description: str, tags: str, size: int, length: int, views: int, like_count: int,
+                  cloudinary_id: int, comments: typing.List[Comment]):
+        return Video(title, description, tags, size, length, views, like_count, cloudinary_id, comments)
 
     @staticmethod
     def from_dict(data: dict):
@@ -64,5 +68,6 @@ class VideoFactory:
             length=data["length"],
             views=data["views"],
             like_count=data["likeCount"],
-            cloudinary_id=data["videoId"]
+            cloudinary_id=data["videoId"],
+            comments=data["comments"]
         )
