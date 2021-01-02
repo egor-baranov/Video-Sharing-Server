92a84a73c0900c0fda72dc2f27161d537ea54114
0x5d0 <49813134+egor-baranov@users.noreply.github.com>
2021-01-02 16:52:43 +0300

    🔨 Some fixes and improvements in Video.py ✨ Added method from_dict to VideoFactory

--- data/Video.py
+++ data/Video.py
@@ -23,6 +23,7 @@ class Video:
                  size: int = 0,
                  length: int = 0,
                  views: int = 0,
+                 like_count: int = 0,
                  cloudinary_id: int = 0):
         self.title = title
         self.description = description
@@ -30,8 +31,8 @@ class Video:
         self.size = size
         self.length = length
         self.views = views
+        self.likes = like_count
         self.cloudinary_id = cloudinary_id
-        self.likes = 0
 
     def to_dict(self):
         return {"title": self.title,
@@ -50,5 +51,18 @@ class VideoFactory:
         return Video()
 
     @staticmethod
-    def new_video(title: str, description: str, tags: str, size: int, length: int, views: int, cloudinary_id: int):
-        return Video(title, description, tags, size, length, views, cloudinary_id)
+    def new_video(title: str, description: str, tags: str, size: int, length: int, views: int, like_count: int, cloudinary_id: int):
+        return Video(title, description, tags, size, length, views, like_count, cloudinary_id)
+
+    @staticmethod
+    def from_dict(data: dict):
+        return VideoFactory.new_video(
+            title=data["title"],
+            description=data["description"],
+            tags=data["tags"],
+            size=data["size"],
+            length=data["length"],
+            views=data["views"],
+            like_count=data["likeCount"],
+            cloudinary_id=data["videoId"]
+        )
