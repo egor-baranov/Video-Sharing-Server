2a53a1eb2873ea902849687be1fe153d7d285f85
0x5d0 <49813134+egor-baranov@users.noreply.github.com>
2021-01-02 09:23:06 +0300

    ✨ Created core.py in components/ and moved there some data

---
+++ components/core.py
@@ -0,0 +1,14 @@
+from flask import Flask
+from flask_cors import *
+
+from components.config import *
+
+app = Flask(__name__)
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
+
+headers = {
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Credentials": "true",
+    "Access-Control-Allow-Methods": "GET, POST, OPTIONS"
+}
