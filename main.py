798fbecc507e61ff488399028fec14a5cea3a508
0x5d0 <49813134+egor-baranov@users.noreply.github.com>
2021-01-20 15:56:37 +0300

    ✨ Added count parameter to /userList request

--- main.py
+++ main.py
@@ -93,8 +93,15 @@ def video_list():
 @app.route("/userList")
 @cross_origin()
 def user_list():
+    count = int(request.args.get("count"))
+
+    resp_list = [u.to_dict() for u in DatabaseWorker.read_users()]
+
+    if len(resp_list) > count:
+        resp_list = resp_list[:count]
+
     resp = make_response(jsonify({
-        "users": [u.to_dict() for u in DatabaseWorker.read_users()]
+        "users": resp_list
     }))
     resp.headers = headers
     return resp
@@ -366,6 +373,7 @@ def block_user():
 
     resp.headers = headers
     return resp
+
 
 @app.route("/restoreUser")
 @cross_origin()
