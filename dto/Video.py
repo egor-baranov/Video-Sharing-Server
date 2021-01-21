15dcaf30038617f5a7058395f493cb7f52ccefa3
0x5d0 <49813134+egor-baranov@users.noreply.github.com>
2021-01-21 21:00:31 +0300

    ✨ Implemented Video.upload_time

--- dto/Video.py
+++ dto/Video.py
@@ -1,5 +1,6 @@
 import random
 import typing
+import time
 
 from dto.Comment import Comment
 
@@ -13,6 +14,7 @@ class Video:
     views: int
     likes: int
     video_id: int
+    upload_time: float
 
     # todo: should be refactored to List[Comment]
     comments: typing.List[str] = []
@@ -26,7 +28,8 @@ class Video:
                  views: int = 0,
                  like_count: int = 0,
                  video_id: int = 0,
-                 comments: typing.List[str] = None):
+                 comments: typing.List[str] = None,
+                 upload_time: float = 0):
         self.title = title
         self.description = description
         self.tags = tags
@@ -36,6 +39,7 @@ class Video:
         self.likes = like_count
         self.video_id = video_id
         self.comments = comments if comments else []
+        self.upload_time = time.time() if upload_time == 0 else upload_time
 
     def to_dict(self):
         return {"title": self.title,
@@ -46,7 +50,9 @@ class Video:
                 "views": self.views,
                 "likeCount": self.likes,
                 "videoId": self.video_id,
-                "comments": self.comments}
+                "comments": self.comments,
+                "uploadTime": self.upload_time
+                }
 
 
 class VideoFactory:
@@ -56,8 +62,8 @@ class VideoFactory:
 
     @staticmethod
     def new_video(title: str, description: str, tags: str, size: int, length: int, views: int, like_count: int,
-                  video_id: int, comments: typing.List[str]):
-        return Video(title, description, tags, size, length, views, like_count, video_id, comments)
+                  video_id: int, comments: typing.List[str], upload_time: float):
+        return Video(title, description, tags, size, length, views, like_count, video_id, comments, upload_time)
 
     @staticmethod
     def from_dict(data: dict):
@@ -70,5 +76,6 @@ class VideoFactory:
             views=data["views"],
             like_count=data["likeCount"],
             video_id=data["videoId"],
-            comments=data["comments"]
+            comments=data["comments"],
+            upload_time=data["uploadTime"]
         )
