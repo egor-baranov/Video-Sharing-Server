2c71984f857db9dc040c30401e64d4cb90fdb1cc
0x5d0 <49813134+egor-baranov@users.noreply.github.com>
2021-01-29 07:42:01 +0300

    🔨 Implementation for user data editing requests

--- routes/admin_panel/user_editing.py
+++ routes/admin_panel/user_editing.py
@@ -2,20 +2,52 @@ from components.core import *
 from flask import make_response, jsonify, request
 from flask_cors import cross_origin
 
+from components.managers.UserManager import UserManager
+
 
 @app.route("/editUserName")
 @cross_origin()
 def edit_user_name():
-    pass
+    email_user = UserManager.get_user_by_email(request.args.get("email"))
+    phone_user = UserManager.get_user_by_phone(request.args.get("phone"))
+
+    if email_user.is_fake() and phone_user.is_fake():
+        resp = make_response(jsonify({"ok": False}))
+        resp.headers = headers
+        return resp
+
+    user = email_user if email_user.is_not_fake() else phone_user
+    user.username = request.args.get("username")
+    UserManager.update_user_data(user)
 
 
 @app.route("/editUserBirthDate")
 @cross_origin()
 def edit_user_birth_date():
-    pass
+    email_user = UserManager.get_user_by_email(request.args.get("email"))
+    phone_user = UserManager.get_user_by_phone(request.args.get("phone"))
+
+    if email_user.is_fake() and phone_user.is_fake():
+        resp = make_response(jsonify({"ok": False}))
+        resp.headers = headers
+        return resp
+
+    user = email_user if email_user.is_not_fake() else phone_user
+    user.birth_date = request.args.get("birthDate")
+    UserManager.update_user_data(user)
 
 
 @app.route("/editUserCity")
 @cross_origin()
 def edit_user_city():
-    pass
+    email_user = UserManager.get_user_by_email(request.args.get("email"))
+    phone_user = UserManager.get_user_by_phone(request.args.get("phone"))
+
+    if email_user.is_fake() and phone_user.is_fake():
+        resp = make_response(jsonify({"ok": False}))
+        resp.headers = headers
+        return resp
+
+    user = email_user if email_user.is_not_fake() else phone_user
+    user.city = request.args.get("city")
+    UserManager.update_user_data(user)
