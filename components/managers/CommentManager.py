63cb37525476874582a7246938e3e6a97f7c3776
0x5d0 <49813134+egor-baranov@users.noreply.github.com>
2021-01-29 03:05:55 +0300

    ✨ read_comments and write_comments in DatabaseWorker ✨ CommentManager.get_comment_by_id method

--- components/managers/CommentManager.py
+++ components/managers/CommentManager.py
@@ -3,4 +3,8 @@ from components.database.dbworker import DatabaseWorker
 
 
 class CommentManager:
-    pass
+    @staticmethod
+    def get_comment_by_id(comment_id: int) -> Comment:
+        for comment in DatabaseWorker.read_comments():
+            if comment.comment_id == comment_id:
+                return comment
