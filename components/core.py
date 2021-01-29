f6a278e1e4347b53aca305b45f07bfdae9af59ba
0x5d0 <49813134+egor-baranov@users.noreply.github.com>
2021-01-29 05:36:51 +0300

    🔨 Add constants to core.py

--- components/core.py
+++ components/core.py
@@ -1,6 +1,8 @@
 from flask import Flask
 from flask_cors import *
 
+from typing import Final
+
 from components.config import *
 
 app = Flask(__name__)
@@ -12,3 +14,7 @@ headers = {
     "Access-Control-Allow-Credentials": "true",
     "Access-Control-Allow-Methods": "GET, POST, OPTIONS",
 }
+
+SECONDS_IN_DAY: Final = 86400
+SECONDS_IN_WEEK: Final = SECONDS_IN_DAY * 7
+SECONDS_IN_MONTH: Final = SECONDS_IN_DAY * 30
