f4ca41dfa740a3124099ab122e5453f60983f382
0x5d0 <49813134+egor-baranov@users.noreply.github.com>
2021-02-02 00:41:25 +0300

    🔨 Moved constants from core.py to config.py

--- components/config.py
+++ components/config.py
@@ -1,6 +1,18 @@
+from typing import Final
+
 users_path = "components/database/data/users.json"
 blocked_users_path = "components/database/data/blocked_users.json"
 
 videos_path = "components/database/data/videos.json"
 
 comments_path = "components/database/data/comments.json"
+
+headers = {
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Credentials": "true",
+    "Access-Control-Allow-Methods": "GET, POST, OPTIONS",
+}
+
+SECONDS_IN_DAY: Final = 86400
+SECONDS_IN_WEEK: Final = SECONDS_IN_DAY * 7
+SECONDS_IN_MONTH: Final = SECONDS_IN_DAY * 30
--- components/core.py
+++ components/core.py
@@ -8,13 +8,3 @@ from components.config import *
 app = Flask(__name__)
 cors = CORS(app)
 app.config["CORS_HEADERS"] = "Content-Type"
-
-headers = {
-    "Access-Control-Allow-Origin": "*",
-    "Access-Control-Allow-Credentials": "true",
-    "Access-Control-Allow-Methods": "GET, POST, OPTIONS",
-}
-
-SECONDS_IN_DAY: Final = 86400
-SECONDS_IN_WEEK: Final = SECONDS_IN_DAY * 7
-SECONDS_IN_MONTH: Final = SECONDS_IN_DAY * 30
