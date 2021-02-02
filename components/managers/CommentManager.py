80b30e00fac4d61fcdd0785cb5f083d4c088e0ac
0x5d0 <49813134+egor-baranov@users.noreply.github.com>
2021-02-02 12:07:35 +0300

    🔨 Changed signature of CommentManager.add_comment method

--- components/managers/CommentManager.py
+++ components/managers/CommentManager.py
@@ -15,18 +15,6 @@ class CommentManager:
                 return comment
 
     @staticmethod
-    @dispatch(User, Video, str)
-    def add_comment(author: User, video: Video, comment_text: str):
-        new_comment = CommentFactory.new_comment(
-            video=video, author=author, replies=[], text=comment_text
-        )
-
-        comments = DatabaseWorker.read_comments()
-        comments.append(new_comment)
-        DatabaseWorker.write_comments(comments)
-
-    @staticmethod
-    @dispatch(Comment)
     def add_comment(c: Comment):
         comments = DatabaseWorker.read_comments()
         comments.append(c)
