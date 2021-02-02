90f325bd53abd3cdd5ac9fb23fa9812adbb3d40d
0x5d0 <49813134+egor-baranov@users.noreply.github.com>
2021-02-02 04:55:18 +0300

    🔨 sortType parameter to /userList function

--- routes/app_requests/data_list.py
+++ routes/app_requests/data_list.py
@@ -3,6 +3,7 @@ from flask import Blueprint
 
 from components.database.dbworker import DatabaseWorker
 from components.core import *
+from dto.User import UserFactory
 
 data_list_blueprint = Blueprint("list", __name__, template_folder="templates", static_folder="static")
 
@@ -31,12 +32,19 @@ def video_list():
 @app.route("/userList")
 @cross_origin()
 def user_list():
-    count = int(request.args.get("count"))
+    count = int(request.args.get("count") if request.args.get("count") is not None else 100)
+
+    sort_type = request.args.get("sortType") if request.args.get("sortType") is not None else "username"
+
+    if sort_type not in UserFactory.new_fake_user().to_dict().keys():
+        sort_type = "username"
 
     resp_list = [u.to_dict() for u in DatabaseWorker.read_users()]
 
     if len(resp_list) > count:
         resp_list = resp_list[:count]
+
+    resp_list.sort(key=lambda x: x[sort_type])
 
     resp = make_response(jsonify({
         "users": resp_list
