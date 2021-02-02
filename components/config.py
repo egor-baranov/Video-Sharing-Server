b2fa3420127ac08a9d341f4794d1b7f9532d5edc
0x5d0 <49813134+egor-baranov@users.noreply.github.com>
2021-02-02 05:37:27 +0300

    🐛 Fixed encodings of json data (here was a problem with utf-8)

--- components/config.py
+++ components/config.py
@@ -11,6 +11,7 @@ headers = {
     "Access-Control-Allow-Origin": "*",
     "Access-Control-Allow-Credentials": "true",
     "Access-Control-Allow-Methods": "GET, POST, OPTIONS",
+    'Content-Type': "application/json; charset=utf-8"
 }
 
 SECONDS_IN_DAY: Final = 86400
--- components/core.py
+++ components/core.py
@@ -6,3 +6,5 @@ from components.config import *
 app = Flask(__name__)
 cors = CORS(app)
 app.config["CORS_HEADERS"] = "Content-Type"
+app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
+app.config['JSON_AS_ASCII'] = False
