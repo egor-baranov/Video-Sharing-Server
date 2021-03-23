82376c9558e3b0daf157df700d430fec1cc88b70
0x5d0 <49813134+egor-baranov@users.noreply.github.com>
2021-03-23 22:43:21 +0300

    ✨ is_phone_valid and is_email_valid functions in core.py

--- components/core.py
+++ components/core.py
@@ -1,3 +1,5 @@
+import re
+
 from flask import Flask
 from flask_cors import *
 
@@ -11,3 +13,13 @@ app.config["JSONIFY_PRETTYPRINT_REGULAR"] = True
 app.config["JSON_AS_ASCII"] = False
 
 smsc = SMSC()
+
+
+def is_phone_valid(phone_number):
+    pattern = re.compile("^[\dA-Z]{3}-[\dA-Z]{3}-[\dA-Z]{4}$", re.IGNORECASE)
+    return pattern.match(phone_number) is not None
+
+
+def is_email_valid(email):
+    pattern = re.compile(r"[^@\s]+@[^@\s]+\.[a-zA-Z0-9]+$")
+    return pattern.match(email) is not None
