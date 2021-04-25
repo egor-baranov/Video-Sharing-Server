a99fdb1934314f2dc4e56acb5e5e3f3cf66fddb9
0x5d0 <49813134+egor-baranov@users.noreply.github.com>
2021-04-26 00:28:41 +0300

    🔨 Changed requests in user_editing.py to userId usage

--- routes/admin_panel/user_editing.py
+++ routes/admin_panel/user_editing.py
@@ -12,15 +12,13 @@ user_editing_blueprint = Blueprint(
 @app.route("/editUserName")
 @cross_origin()
 def edit_user_name():
-    email_user = UserManager.get_user_by_email(request.args.get("email"))
-    phone_user = UserManager.get_user_by_phone(request.args.get("phone"))
+    user = UserManager.get_user_by_id(request.args.get("userId"))
 
-    if email_user.is_fake() and phone_user.is_fake():
+    if user.is_fake():
         resp = make_response(jsonify({"ok": False}))
         resp.headers = headers
         return resp
 
-    user = email_user if email_user.is_not_fake() else phone_user
     user.username = request.args.get("username")
     UserManager.update_user_data(user)
 
@@ -32,15 +30,13 @@ def edit_user_name():
 @app.route("/editUserBirthDate")
 @cross_origin()
 def edit_user_birth_date():
-    email_user = UserManager.get_user_by_email(request.args.get("email"))
-    phone_user = UserManager.get_user_by_phone(request.args.get("phone"))
+    user = UserManager.get_user_by_id(request.args.get("userId"))
 
-    if email_user.is_fake() and phone_user.is_fake():
+    if user.is_fake():
         resp = make_response(jsonify({"ok": False}))
         resp.headers = headers
         return resp
 
-    user = email_user if email_user.is_not_fake() else phone_user
     user.birth_date = request.args.get("birthDate")
     UserManager.update_user_data(user)
 
@@ -52,15 +48,13 @@ def edit_user_birth_date():
 @app.route("/editUserCity")
 @cross_origin()
 def edit_user_city():
-    email_user = UserManager.get_user_by_email(request.args.get("email"))
-    phone_user = UserManager.get_user_by_phone(request.args.get("phone"))
+    user = UserManager.get_user_by_id(request.args.get("userId"))
 
-    if email_user.is_fake() and phone_user.is_fake():
+    if user.is_fake():
         resp = make_response(jsonify({"ok": False}))
         resp.headers = headers
         return resp
 
-    user = email_user if email_user.is_not_fake() else phone_user
     user.city = request.args.get("city")
     UserManager.update_user_data(user)
 
